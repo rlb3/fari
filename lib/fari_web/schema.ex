4e7867ccbef70a5746741fba1d75abe4e9189a73
Robert Boone <robert@rlb3.com>
2018-04-15 09:14:10 -0500

    Make register and login mutations return types non_null

--- lib/fari_web/schema.ex
+++ lib/fari_web/schema.ex
@@ -9,7 +9,7 @@ defmodule FariWeb.Schema do
   end
 
   mutation do
-    field :register, :user do
+    field :register, non_null(:user) do
       arg(:first_name, :string, description: "First name")
       arg(:last_name, :string, description: "Last name")
       arg(:email, non_null(:string), description: "The users email")
@@ -18,7 +18,7 @@ defmodule FariWeb.Schema do
       resolve(&FariWeb.Resolvers.Users.register/3)
     end
 
-    field :login, :session do
+    field :login, non_null(:session) do
       arg(:email, :string, description: "User's email")
       arg(:password, :string, description: "Password")
 
