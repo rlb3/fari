92d58758fc8c70ccdf82ac6b1140c239c6d6a1ef
Robert Boone <robert@rlb3.com>
2018-04-16 07:27:17 -0500

    Refactor

--- lib/fari_web/resolvers/groups.ex
+++ lib/fari_web/resolvers/groups.ex
@@ -3,21 +3,18 @@ defmodule FariWeb.Resolvers.Groups do
   alias Fari.Core.{Group, Membership}
 
   def create(_obj, args, %{context: %{current_user: user}}) do
-    group =
-      %Group{}
-      |> Group.changeset(args)
-      |> Repo.insert()
-
-    case group do
-      {:ok, group} ->
-        %Membership{}
-        |> Membership.changeset(%{admin: true, user_id: user.id, group_id: group.id})
-        |> Repo.insert()
-
-        {:ok, group}
-
+    with {:ok, group} <- %Group{} |> Group.changeset(args) |> Repo.insert(),
+         {:ok, _membership} <-
+           %Membership{}
+           |> Membership.changeset(%{admin: true, user_id: user.id, group_id: group.id})
+           |> Repo.insert() do
+      {:ok, group}
+    else
       {:error, %{errors: [name: {message, _}]}} ->
         {:error, "Name #{message}"}
+
+      {:error, message} ->
+        {:error, message}
     end
   end
 
